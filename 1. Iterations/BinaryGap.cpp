45b7b11eb60461708efbce65b4f4370934767cbd
Nayeem Reza <reza.uranium@gmail.com>
2018-12-24 20:58:31 +0600

    make the solution modular

--- 1. Iterations/BinaryGap.cpp
+++ 1. Iterations/BinaryGap.cpp
@@ -2,25 +2,30 @@
 
 using namespace std;
 
+int solution(in N) {
+    int pointer = -1, maxDiff = 0;
+    for(int i=0; N; i++){
+        int bit = N & 1;
+        if(bit){
+            if(pointer == -1){
+                pointer = i;
+            } else {
+                int diff = i - pointer - 1;
+                if (diff > maxDiff) maxDiff = diff;
+                pointer = i;
+            }
+        }
+        N >>= 1;
+    }
+    
+    return maxDiff;
+}
+
 int main()
 { 
     int number;
     while(cin>>number){    
-        int pointer = -1, maxDiff = 0;
-        for(int i=0; number; i++){
-            int bit = number & 1;
-            if(bit){
-                if(pointer == -1){
-                    pointer = i;
-                } else {
-                    int diff = i - pointer - 1;
-                    if (diff > maxDiff) maxDiff = diff;
-                    pointer = i;
-                }
-            }
-            number >>= 1;
-        }
-        cout << maxDiff << endl;
+        cout << solution(N) << endl;
     }
     return 0;
 }
